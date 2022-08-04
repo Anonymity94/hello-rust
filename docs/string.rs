07ff2b0adb4eb854f9f04528b99382d5b5a1d405
Anonymity94 <1176989591@qq.com>
2022-08-04 18:39:51 +0800

    feat: str

--- docs/string.rs
+++ docs/string.rs
@@ -1,7 +1,16 @@
+// rustc docs/string.rs && ./string
+
 fn main() {
     // char 类型，必须用单引号包裹
     let char = '我';
     println!("{}是 char 类型", char);
+
+    // &str 类型
+    // 其中str表示str类型，&表示该类型的引用，即一个指针
+    let str1 = "你好啊派大星。";
+    // 等价于
+    let str2: &str = "你好啊派大星。2";
+    println!("{},{}", str1, str2);
 
     // 定义 string
     let mut string = String::from("Hello");
